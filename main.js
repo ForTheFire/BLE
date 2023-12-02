caec7d066e87c165fa00e3580d63a7146023da81
BuildTools <unconfigured@null.spigotmc.org>
2023-12-02 19:37:04 +0100

    main1

--- main.js
+++ main.js
@@ -1,56 +1,18 @@
-const button = document.getElementById("getDetails");
-const details = document.getElementById("details");
+function requestDevice() {
+    let options = {
+        acceptAllDevices: true
+    };
 
+    navigator.bluetooth.requestDevice(options)
+        .then(device => {
+            let devicesList = document.getElementById('devices-list');
+            let listItem = document.createElement('li');
+            listItem.textContent = device.name;
+            devicesList.appendChild(listItem);
+        })
+        .catch(error => {
+            console.error(`Device request failed: ${error}`);
+        });
+}
 
-const device = await navigator.bluetooth.requestDevice({
-    optionalServeces: ["battery_service", "device_information"],
-    acceptAllDevices: true,
-});
-let device_name = device.gatt.device.name;
-const server = await device.gatt.connect();
-const batteryService = await server.getPrimaryService("battery_service");
-const infoService = await server.getPrimaryService("device_information");
-
-// Getting the current battery level
-const batteryLevelCharacteristic = await batteryService.getCharacteristic(
-    "battery_level"
-  );
-// Convert recieved buffer to number
-const batteryLevel = await batteryLevelCharacteristic.readValue();
-const batteryPercent = await batteryLevel.getUint8(0);
-// Getting device information
-// We will get all characteristics from device_information
-const infoCharacteristics = await infoService.getCharacteristics();
-console.log(infoCharacteristics);
-let infoValues = [];
-const promise = new Promise((resolve, reject) => {
-  infoCharacteristics.forEach(async (characteristic, index, array) => {
-    // Returns a buffer
-    const value = await characteristic.readValue();
-    console.log(new TextDecoder().decode(value));
-    // Convert the buffer to string
-    infoValues.push(new TextDecoder().decode(value));
-    if (index === array.length - 1) resolve();
-  });
-});
-
-promise.then(() => {
-    // Display all the information on the screen
-    // use innerHTML
-    details.innerHTML = `
-      Device Name - ${deviceName}<br />
-      Battery Level - ${batteryPercent}%<br />
-      Device Information:
-      <ul>
-        ${infoValues.map((value) => `<li>${value}</li>`).join("")}
-      </ul> 
-    `;
-  });
-
-button.addEventListener("click", async () => {
-    try {
-    } catch(err){
-        console.error();
-        alert("Error");
-    }
-})
+document.getElementById('available-devices').addEventListener('click', requestDevice);
